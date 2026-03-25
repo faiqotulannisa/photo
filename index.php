7642507f7b0de9b5a84e7711a2e13bbf38b21f66
Kurniawan Jatmika <munirjb@gmail.com>
2026-03-25 16:37:03 +0700

    add google tag manager code

--- index.php
+++ index.php
@@ -215,6 +215,15 @@ h2 {
     0 0 40px #ff00ff;
 }
 </style>
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-76WSF3EY3P"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+  
+    gtag('config', 'G-76WSF3EY3P');
+  </script>
 </head>
 
 <body>
